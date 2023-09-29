594c85c1ad29dbc936ccc15aa3402b00fb454ce3
ZbyszekZebrowski <137501903+ZbyszekZebrowski@users.noreply.github.com>
2023-09-29 18:06:23 +0300

    Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = new Player('vimeo-player');
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+if (savedTime) {
+  vimeoPlayer.setCurrentTime(parseFloat(savedTime));
+}
+
+
+vimeoPlayer.on('timeupdate', throttle((data) => {
+  const currentTime = data.seconds;
+  localStorage.setItem(STORAGE_KEY, currentTime);
+}, 1000)); 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+function saveFormData() {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function loadFormData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+}
+
+emailInput.addEventListener('input', throttle(saveFormData, 500));
+messageInput.addEventListener('input', throttle(saveFormData, 500));
+
+loadFormData();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+});
 
