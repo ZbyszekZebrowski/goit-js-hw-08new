dae10bf9ba7238a6f564dac3b364ed6b4cd9a6f6
ZbyszekZebrowski <137501903+ZbyszekZebrowski@users.noreply.github.com>
2023-09-29 17:54:04 +0300

    gallery done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const ulGallery = document.querySelector('.gallery');
+const addGallary = createGallery(galleryItems);
+
+ulGallery.innerHTML = addGallary;
+
+function createGallery(images) {
+  return images
+    .map(
+      image =>
+        `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+                <img class="gallery__image" src="${image.preview}" alt="${image.description}" title="${image.description}" />
+            </a>
+        </li>
+        `
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
